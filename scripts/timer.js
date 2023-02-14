ad892e5c5259b447dd8f179cf2e867205b84c4ec
Carl-Johan Wähler <cjwaehler@gmail.com>
2023-02-14 09:53:52 +0100

    Save and restore more than 1 timer

--- scripts/timer.js
+++ scripts/timer.js
@@ -15,17 +15,28 @@ let sound = localStorage.getItem('audio');
 let soundSelection = document.getElementById('sounds');
 let menu = false;
 let p = document.querySelectorAll('p');
+let timerMenu = false;
+const timerSelection = document.getElementById('savedTimers');
+const timerSelectionContent = document.getElementById('boxContent');
+timerSelectionContent.innerHTML = localStorage.getItem('timers');
+const nav = document.querySelector('nav');
+const footerbar = document.querySelector('footer');
 
 let save = document.getElementById('save');
 let restore = document.getElementById('restore');
+let timerCounter = localStorage.getItem('timerCounter');
+if (timerSelectionContent.innerHTML === '') {
+    timerCounter = 0;
+}
 
-if(sound === 'audio3') {
+
+if (sound === 'audio3') {
     alarm = sound3;
     p[2].classList.add('active');
-} else if(sound === 'audio2') {
+} else if (sound === 'audio2') {
     alarm = sound2;
     p[1].classList.add('active');
-} else{
+} else {
     alarm = sound1;
     p[0].classList.add('active');
 }
@@ -40,7 +51,7 @@ function startTimer() {
     let now = new Date().getTime();
     let endTime = now + timeResult;
     reset.classList.remove('unsichtbar');
-    if(!resetTime) {
+    if (!resetTime) {
         minutesText = minutesInput.value;
         secondsText = secondsInput.value;
     }
@@ -52,8 +63,8 @@ function startTimer() {
     soundSelection.classList.add('unsichtbar');
     menu = false;
 
-    let x = setInterval(function() {
-        pause.addEventListener('click', function() {
+    let x = setInterval(function () {
+        pause.addEventListener('click', function () {
             start.classList.remove('unsichtbar');
             pause.classList.add('unsichtbar');
             minutesInput.style.pointerEvents = 'all';
@@ -64,7 +75,7 @@ function startTimer() {
             restore.classList.remove('disabled');
         })
 
-        end.addEventListener('click', function() {
+        end.addEventListener('click', function () {
             start.classList.remove('unsichtbar');
             minutesInput.style.pointerEvents = 'all';
             minutesInput.value = '00';
@@ -79,7 +90,7 @@ function startTimer() {
             restore.classList.remove('disabled');
         })
 
-        reset.addEventListener('click', function() {
+        reset.addEventListener('click', function () {
             minutesInput.value = minutesText;
             secondsInput.value = secondsText;
             minutesInput.style.pointerEvents = 'all';
@@ -104,7 +115,7 @@ function startTimer() {
             if (seconds < 10) {
                 secondsInput.value = '0' + seconds;
             } else if (seconds === 60) {
-                minutes +=1;
+                minutes += 1;
                 minutesInput.value = minutes;
                 secondsInput.value = '00';
             } else {
@@ -131,43 +142,50 @@ function startTimer() {
     }, 1000);
 }
 
-function saveTimer(){
-    alert('Es wird jetzt folgende Zeit eingespeichert: ' + minutesInput.value + ' : ' + secondsInput.value);
-    localStorage.setItem('minutes', minutesInput.value);
-    localStorage.setItem('seconds', secondsInput.value);
-}
-
-function restoreTime() {
-    minutesInput.value = localStorage.getItem('minutes');
-    secondsInput.value = localStorage.getItem('seconds');
+function saveTimer() {
+    timerCounter++;
+    if (minutesInput.value < 10 && minutesInput.value[0] !== '0') {
+        minutesInput.value = 0 + minutesInput.value;
+    } else if (minutesInput.value === '0') {
+        minutesInput.value += 0;
+    }
+    if (secondsInput.value < 10 && secondsInput.value[0] !== '0') {
+        secondsInput.value = 0 + secondsInput.value;
+    } else if (secondsInput.value === 0) {
+        secondsInput.value += 0;
+    }
+    timerSelectionContent.innerHTML += `<p onclick='selectTimer(${timerCounter})' id='timer${timerCounter}'><span id='minutes${timerCounter}'>${minutesInput.value}</span> : <span id='seconds${timerCounter}'>${secondsInput.value}</span><span onclick='removeTimer(${timerCounter})' class='remove' title='Timer löschen'>X</span></p>`
+    localStorage.setItem('timers', timerSelectionContent.innerHTML);
+    localStorage.setItem('timerCounter', timerCounter);
+    alert('Folgende Zeit wurde erfolgreich eingespeichert: ' + minutesInput.value + ' : ' + secondsInput.value);
 }
 
 function soundMenu() {
-    if(!menu) {
+    if (!menu) {
         soundSelection.classList.remove('unsichtbar');
         menu = true;
-    } else{
+    } else {
         soundSelection.classList.add('unsichtbar');
         menu = false;
     }
 }
 
 function chooseSound(soundTrack) {
-    if(soundTrack === 1) {
+    if (soundTrack === 1) {
         alarm = sound1;
         alarm.play();
         sound = localStorage.setItem('audio', 'audio1');
         p[0].classList.add('active');
         p[1].classList.remove('active');
         p[2].classList.remove('active');
-    } else if(soundTrack === 2) {
+    } else if (soundTrack === 2) {
         alarm = sound2;
         alarm.play();
         sound = localStorage.setItem('audio', 'audio2');
         p[1].classList.add('active');
         p[0].classList.remove('active');
         p[2].classList.remove('active');
-    } else{
+    } else {
         alarm = sound3;
         alarm.play();
         sound = localStorage.setItem('audio', 'audio3');
@@ -175,4 +193,48 @@ function chooseSound(soundTrack) {
         p[0].classList.remove('active');
         p[1].classList.remove('active');
     }
+}
+
+function openTimerSelection() {
+    if (!timerMenu) {
+        timerSelection.classList.remove('unsichtbar');
+        timerMenu = true;
+        nav.classList.add('disabled');
+        footerbar.classList.add('disabled');
+        secondsInput.classList.add('disabled');
+    } else {
+        timerSelection.classList.add('unsichtbar');
+        timerMenu = false;
+        nav.classList.remove('disabled');
+        footerbar.classList.remove('disabled');
+        secondsInput.classList.remove('disabled');
+    }
+}
+
+function selectTimer(timerNumber) {
+    let minutes = document.getElementById('minutes' + timerNumber).innerHTML;
+    let seconds = document.getElementById('seconds' + timerNumber).innerHTML;
+    minutesInput.value = minutes;
+    secondsInput.value = seconds;
+    resetTime = false;
+    openTimerSelection();
+}
+
+function removeTimer(timerNumber) {
+    let selectedTimer = document.getElementById('timer' + timerNumber);
+    selectedTimer.remove();
+    if (timerSelectionContent.innerHTML === '') {
+        localStorage.removeItem('timerCounter');
+        localStorage.removeItem('timers');
+        timerCounter = 0;
+    } else {
+        localStorage.setItem('timers', timerSelectionContent.innerHTML);
+    }
+}
+
+function deleteAllTimers() {
+    timerSelectionContent.innerHTML = '';
+    timerCounter = 0;
+    localStorage.removeItem('timerCounter');
+    localStorage.removeItem('timers');
 }

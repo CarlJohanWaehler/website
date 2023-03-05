1b42399af4c33a16175ec70e4adbf3f98366a6b8
Carl-Johan Wähler <cjwaehler@gmail.com>
2023-03-05 10:43:29 +0100

    Improve notelist + add article about "Microsoft PowerToys"

--- scripts/notes.js
+++ scripts/notes.js
@@ -6,14 +6,26 @@ let fieldOpener = document.getElementById('fieldOpener');
 let deleteAllButton = document.getElementById('deleteAll');
 let message = document.getElementById('message');
 let cancel = document.getElementById('cancel');
+let actualEntry = '';
+const palette = document.getElementById('palette');
 
 notes.innerHTML = localStorage.getItem('notelist');
 let counter = localStorage.getItem('count');
+let selectionStart = '';
+let selectionEnd = '';
 
 if (notes.innerHTML === '') {
     deleteAllButton.classList.add('toggleUnvisible');
     counter = 0;
 };
+
+newNote.addEventListener('select', function () {
+    selectionStart = newNote.selectionStart;
+    selectionEnd = newNote.selectionEnd;
+    if(selectionStart === selectionEnd) {
+        selectionStart = '';
+    }
+});
 
 function noteField() {
     if (newNote.classList.contains('toggleUnvisible')) {
@@ -23,6 +35,7 @@ function noteField() {
         save.classList.remove('toggleUnvisible');
         cancel.classList.remove('toggleUnvisible');
         notes.style.pointerEvents = 'none';
+        palette.classList.remove('toggleUnvisible');
         cancel.addEventListener('click', function () {
             newNote.value = '';
             save.classList.add('toggleUnvisible');
@@ -31,10 +44,43 @@ function noteField() {
             newNote.classList.add('toggleUnvisible');
             notes.style.pointerEvents = 'all';
             cancel.classList.add('toggleUnvisible');
+            palette.classList.add('toggleUnvisible');
             if (notes.innerHTML === '') {
                 deleteAllButton.classList.add('toggleUnvisible');
             };
         })
+    }
+}
+
+function specialStyle(element) {
+    let field = '';
+    if (editActive) {
+        field = document.getElementById('editNode' + actualEntry);
+    } else {
+        field = newNote;
+    }
+    if (selectionStart !== '') {
+        let textBefore = field.value.substring(0, selectionStart);
+        let selected = field.value.substring(selectionStart, selectionEnd);
+        let textAfter = field.value.substring(selectionEnd);
+        field.value = field.value.slice(selectionStart, 0) + textBefore + '<' + element + '>' + selected + '</' + element + '>' + textAfter;
+        selectionStart = '';
+        selectionEnd = '';
+    } else {
+        //content = field.value;
+        field.value += '<' + element + '></' + element + '>';
+        let index = '';
+
+        if (element === 'h1' || element === 'h2' || element === 'h3') {
+            index = field.value.length - 5;
+        } else if (element === 'sub' || element === 'sup') {
+            index = field.value.length - 6;
+        } else {
+            index = field.value.length - 4;
+        }
+        field.focus();
+        field.setSelectionRange(index, index);
+        selectionStart = '';
     }
 }
 
@@ -65,7 +111,7 @@ function addNote() {
         let year = miliseconds.getUTCFullYear();
         let today = day + '.' + month + '.' + year;
         let content = newNote.value;
-        content = content.replace(/\n\r?/g, '<br/>');
+        content = content.replace(/\n\r?/g, '<br>');
         notes.innerHTML += `<div id='entry${counter}' class='notesEntry'><span id='time'>${today} ${timeLeft}</span><span id='note${counter}'>${content}</span><img src="../img/edit_note.svg" alt="" class="button" id="edit${counter}" title="Notiz bearbeiten" onclick="editNote(${counter})">
         <img src="../img/delete.svg" alt="" class="button" id="delete${counter}" onclick="deleteNote(${counter})" title="Notiz löschen"><br><p id='changeMessage${counter}' class='changes'></p>
         </div>`
@@ -77,6 +123,9 @@ function addNote() {
         notes.style.pointerEvents = 'all';
         localStorage.setItem('notelist', notes.innerHTML);
         localStorage.setItem('count', counter);
+        selectionStart = '';
+        selectionEnd = '';
+        palette.classList.add('toggleUnvisible');
     } else {
         alert('Bitte füge Text in das Eingabefeld! Du kannst keine Notizen ohne Inhalt anlegen!');
     }
@@ -89,6 +138,8 @@ function deleteNote(number) {
         note.remove();
         fieldOpener.classList.remove('toggleUnvisible');
         deleteAllButton.classList.remove('toggleUnvisible');
+        palette.classList.add('toggleUnvisible');
+        actualEntry = '';
     }
     if (notes.innerHTML === '') {
         localStorage.removeItem('count');
@@ -103,22 +154,37 @@ function deleteNote(number) {
 function editNote(number) {
     if (!editActive) {
         editActive = true;
+        actualEntry = number;
+        palette.classList.remove('toggleUnvisible');
         let note = document.getElementById('note' + number);
         fieldOpener.classList.add('toggleUnvisible');
         let editButton = document.getElementById('edit' + number);
         editButton.classList.add('toggleUnvisible');
         deleteAllButton.classList.add('toggleUnvisible');
-        let content = note.innerText;
+        let content = note.innerHTML;
+        for (let i = 0; i < content.length; i++) {
+            content = content.replace('<br>', '\n');
+        }
         note.innerHTML = `<textarea class='editFields' id='editNode${number}' name='' placeholder='Notiz hier einfügen ...'>${content}</textarea><img src="../img/check.svg" alt="" class="button" title="Änderungen abspeichern" onclick="editFinish(${number})"><img src="../img/close.svg" alt="" class="button" id="cancelEdit" title="Abbrechen">`
         let cancelEdit = document.getElementById('cancelEdit');
+        const editNode = document.getElementById('editNode' + number);
+        editNode.addEventListener('select', function () {
+            selectionStart = editNode.selectionStart;
+            selectionEnd = editNode.selectionEnd;
+            if(selectionStart === selectionEnd) {
+                selectionStart = '';
+            }
+        });
         cancelEdit.addEventListener('click', function () {
             content = content.replace(/\n\r?/g, '<br/>');
             note.innerHTML = content;
             editButton.classList.remove('toggleUnvisible');
             deleteAllButton.classList.remove('toggleUnvisible');
             fieldOpener.classList.remove('toggleUnvisible');
+            palette.classList.add('toggleUnvisible');
             editActive = false;
             cancelEdit.remove();
+            actualEntry = '';
         });
     }
 }
@@ -161,6 +227,8 @@ function editFinish(number) {
     let today = day + '.' + month + '.' + year;
     changeMessage.innerHTML = 'Notiz zuletzt geändert am ' + today + ' um ' + timeLeft + '.';
     localStorage.setItem('notelist', notes.innerHTML);
+    actualEntry = '';
+    palette.classList.add('toggleUnvisible');
 }
 
 function deleteAll() {
